e893f360a03423b0f5be69ffd0366abb957b02df
batman37222222222222 <146122683+batman37222222222222@users.noreply.github.com>
2023-09-26 14:33:22 -0400

    Add files via upload

---
+++ PLHW1_woodsvijay.py
@@ -0,0 +1,14 @@
+# Vijay Woods
+#9/26/23
+#Input, ouput, expression
+
+num1=int(input("Enter a number:"))
+
+print("you Entered", num1)
+print(num1, "square is ",num1**2)
+print("And", num1, "cubed is ",num1**3)
+
+num2=int(input("Enter another number:"))
+
+print(num1,"+", num2, "=",num1+num2)
+print(num1, "*", num2, "=",num1*num2)
