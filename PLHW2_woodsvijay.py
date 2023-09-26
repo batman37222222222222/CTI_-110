454accc52d5ee94f80a67c6d4f9e41dceac05aa4
batman37222222222222 <146122683+batman37222222222222@users.noreply.github.com>
2023-09-26 14:36:38 -0400

    Add files via upload

---
+++ PLHW2_woodsvijay.py
@@ -0,0 +1,23 @@
+#vijay
+#Date
+#Calculates travel expenses
+
+budget=int(input("Enter Budget: "))
+
+dest=input("where are you taveling?")
+
+gas=int(input("Enter gas budget: "))
+food=int(input("Enter food budget: "))
+hotel=int(input("Enter hotel Budget: "))
+
+expenes= gas+food+hotel
+
+print("-----------Travel Expenses--------")
+print("Location: ", dest)
+print("Initial Budget: ",budget)
+print()
+print("Gas Cost: ",gass)
+print("Gas Food:", food)
+print("Gas Hotel:",hotel)
+print()
+print("Remaining Balance: ", budget-expense)
