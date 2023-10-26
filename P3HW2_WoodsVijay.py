2fa205e28f76b1b741fa2e967ddcab2693c0eb2f
batman37222222222222 <146122683+batman37222222222222@users.noreply.github.com>
2023-10-26 14:37:00 -0400

    Add files via upload

---
+++ P3HW2_WoodsVijay.py
@@ -0,0 +1,33 @@
+#Vijay Woods
+#10/26/23
+#Uses if/else statements
+
+#Get employee name from user
+employeename = input("Enter your employee name: ")
+#Get number of hours from user
+hours = int(input("numbers of hours: "))
+#Get payrate per hour from user
+payrate= float(input(" Enter payrate per hours: "))
+#Determine if employee worked more than 40 hours
+#Calculate reg hours worked
+#Calculate OT hours
+if hours > 40:
+    OT= hours - 40
+
+    reghours = 40
+else:
+    OT = 0
+    reghours = hours
+
+
+#Calculate OT Pay
+OTpay  = OT*(payrate*1.5)
+regpay = reghours*(payrate)
+# Display name, payrate, reg hours, OT hours, gross pay
+print("Name: " ,employeename)
+print("Payrate: " , payrate)
+print("Reghour: " , reghours)
+print("Regpay: ",  regpay)
+print("OThours : ", OT)
+print("OTpay: ", OTpay)
+print("grosspay", regpay + OTpay)
